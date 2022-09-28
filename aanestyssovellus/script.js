602f47e2d80ed3714e2d287685b4d74c2ab52f6e
juhr01 <juho1.riipinen@gmail.com>
2022-09-28 12:19:47 +0300

    napit

--- aanestyssovellus/script.js
+++ aanestyssovellus/script.js
@@ -1,17 +1,22 @@
+const kirjautunut = false;
+
 function login() {
     if (document.querySelector("#adminword").value == "orava") {
         document.querySelector("#adminword").style.display = "none";
         document.querySelector("#login").style.display = "none";
-        document.querySelector("#logout").style.display = "block";
-        document.querySelector("#kirjautuneena").style.display = "block";
+        document.querySelector("#logout").style.display = "inline-block";
+        document.querySelector("#kirjautuneena").style.display = "inline-block";
         document.querySelector("#logginglabel").style.display = "none";
+        kirjautunut = true;
     }
 }
 
 function logout() {
-    document.querySelector("#adminword").style.display = "block";
-    document.querySelector("#login").style.display = "block";
+    document.querySelector("#adminword").style.display = "inline-block";
+    document.querySelector("#login").style.display = "inline-block";
     document.querySelector("#logout").style.display = "none";
     document.querySelector("#kirjautuneena").style.display = "none";
-    document.querySelector("#logginglabel").style.display = "block";
+    document.querySelector("#logginglabel").style.display = "inline-block";
+    kirjautunut = false;
 }
+

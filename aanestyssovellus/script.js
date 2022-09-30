d24012b4e05c98f701af7795d6fec97588be362f
juhr01 <juho1.riipinen@gmail.com>
2022-09-30 13:08:03 +0300

    luo äänestys ruutu

--- aanestyssovellus/script.js
+++ aanestyssovellus/script.js
@@ -8,7 +8,6 @@ function login() {
         document.querySelector("#kirjautuneena").style.display = "inline-block";
         document.querySelector("#logginglabel").style.display = "none";
         document.querySelector("#create").style.display = "inline-block";
-        document.querySelector("#delete").style.display = "inline-block";
         kirjautunut = true;
     }
 }
@@ -20,7 +19,23 @@ function logout() {
     document.querySelector("#kirjautuneena").style.display = "none";
     document.querySelector("#logginglabel").style.display = "inline-block";
     document.querySelector("#create").style.display = "none";
-    document.querySelector("#delete").style.display = "none";
     kirjautunut = false;
 }
 
+function createVote() {
+    document.querySelector("#createVote").style.display = "block";
+    document.querySelector("#create").style.display = "none";
+    document.querySelector("#votes").style.display = "none";
+}
+
+function exitCreate() {
+    document.querySelector("#createVote").style.display = "none";
+    document.querySelector("#create").style.display = "inline-block";
+    document.querySelector("#votes").style.display = "block";
+}
+
+function finishCreate() {
+    document.querySelector("#createVote").style.display = "none";
+    document.querySelector("#create").style.display = "inline-block";
+    document.querySelector("#votes").style.display = "block";
+}

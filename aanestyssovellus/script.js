acb04522b0deb94cd27d7eea1968af35b5082501
juhr01 <juho1.riipinen@gmail.com>
2022-10-04 13:52:31 +0300

    etenee

--- aanestyssovellus/script.js
+++ aanestyssovellus/script.js
@@ -1,5 +1,10 @@
 const kirjautunut = false;
 
+if (window.localStorage.getItem('votes') == null) {
+     var votes = [];
+    window.localStorage.setItem('votes', JSON.stringify(votes));
+}
+
 function login() {
     if (document.querySelector("#adminword").value == "orava") {
         document.querySelector("#adminword").style.display = "none";
@@ -7,7 +12,7 @@ function login() {
         document.querySelector("#logout").style.display = "inline-block";
         document.querySelector("#kirjautuneena").style.display = "inline-block";
         document.querySelector("#logginglabel").style.display = "none";
-        document.querySelector("#create").style.display = "inline-block";
+        document.querySelector("#createBtn").style.display = "inline-block";
         kirjautunut = true;
     }
 }
@@ -18,19 +23,19 @@ function logout() {
     document.querySelector("#logout").style.display = "none";
     document.querySelector("#kirjautuneena").style.display = "none";
     document.querySelector("#logginglabel").style.display = "inline-block";
-    document.querySelector("#create").style.display = "none";
+    document.querySelector("#createBtn").style.display = "none";
     kirjautunut = false;
 }
 
 function createVote() {
     document.querySelector("#createVote").style.display = "block";
-    document.querySelector("#create").style.display = "none";
+    document.querySelector("#createBtn").style.display = "none";
     document.querySelector("#votes").style.display = "none";
 }
 
 function exitCreate() {
     document.querySelector("#createVote").style.display = "none";
-    document.querySelector("#create").style.display = "inline-block";
+    document.querySelector("#createBtn").style.display = "inline-block";
     document.querySelector("#votes").style.display = "block";
 }
 
@@ -39,8 +44,55 @@ function finishCreate() {
         alert("Täytäthän kaikki kentät!");
     } else {
     document.querySelector("#createVote").style.display = "none";
-    document.querySelector("#create").style.display = "inline-block";
+    document.querySelector("#createBtn").style.display = "inline-block";
     document.querySelector("#votes").style.display = "block";
+
+    let question = document.querySelector("question").value;
+    let option1 = document.querySelector("option1").value;
+    let option2 = document.querySelector("option2").value;
+
+    let vote = {voteQuestion:question, voteOptions: [{"optionName:" : option1, "votes" : 0},{"optionName" : option2, "votes" : 0}]};
+
+    let votes = JSON.parse(window.localStorage.getItem('aanestykset'));
+    votes.push(vote);
+    window.localStorage.setItem('votes', JSON.stringify(votes));
     }
 
 }
+function getVotes() {
+    let newVoteDiv = document.createElement('div');
+    document.querySelector('#votes').innerHTML = "";
+    newVoteDiv.innerHTML = "";
+    newVoteDiv.className = "votes";
+    newVoteDiv.style.display = "block";
+    let votes = JSON.parse(window.localStorage.getItem('votes'));
+    var voteNumber = 0;
+
+    votes.forEach(vote =>{
+        let delBtn = document.createElement('button');
+        let delBtnText = document.createTextNode('Poista');
+        delBtn.className = "controlBtn";
+        let voteH2 = document.createElement('h2');
+        let voteQuestion = document.createTextNode(vote.question);
+        voteH2.appendChild(voteQuestion);
+
+        delBtn.addEventListener('click', delClick);
+        delBtn.appendChild(delBtnText);
+        newVoteDiv.appendChild(delBtn);
+
+        let optionList = document.createElement('ul');
+        let optionNumber = 0;
+
+        vote.voteOptions.forEach(option => {
+            let optionElement = document.createElement('li');
+            let optionText1 = document.createElement('h3');
+            let optionText2 = document.createTextNode(option.optionName);
+            optionText1.appendChild(optionText2);
+            optionElement.appendChild(optionText1);
+
+            
+
+        })
+    })
+}
+
